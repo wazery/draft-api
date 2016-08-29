1e33a50f9509e84a9cc084fd9aec00cba7cd0d87
Islam Wazery <wazery@ubuntu.com>
2016-08-29 03:02:02 +0200

    Fix redundunt extra last line, and missing callback deletion

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -1,6 +1,4 @@
 class MiscController < ApplicationController
-  skip_before_filter :api_session_token_authenticate!
-
   def ping
     render json: { status: 'OK' }
   end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -69,4 +69,3 @@
 #     api_sessions_path(token)
 #   end
 # end
-#
