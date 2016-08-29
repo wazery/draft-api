23c9239e87a04b07ab8b0e7af5c4dfa12fbe1142
Islam Wazery <wazery@ubuntu.com>
2016-08-29 02:30:59 +0200

    Fix double quotes on conf/application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,16 +1,16 @@
 require_relative 'boot'
 
-require "rails"
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "action_cable/engine"
-# require "sprockets/railtie"
-require "rails/test_unit/railtie"
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+require 'action_cable/engine'
+# require 'sprockets/railtie'
+# require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -26,6 +26,18 @@ module DraftAppApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Insert some middlewares in order for OmniAuth to work
+    config.middleware.use ActionDispatch::Flash
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+
+    # Setup Active Job Adapter
+    config.active_job.queue_adapter = :sidekiq
+
+    # Use selective stack middleware to enable the session middleware when
+    # the URL does not start with /api/
+    # config.middleware.insert_before ActionDispatch::ParamsParser, 'SelectiveStack'
 
     # Rack Cors configuration
     config.middleware.use Rack::Cors do
