8561de665d973d06f98681048ed8af1a199fbfa1
Islam Wazery <wazery@ubuntu.com>
2016-10-17 05:40:33 +0200

    Add a missing ivar to mailer view

--- app/mailers/beta_mailer.rb
+++ app/mailers/beta_mailer.rb
@@ -1,6 +1,7 @@
 class BetaMailer < ApplicationMailer
   def welcome_email(beta_requester)
     @beta_requester = beta_requester
+    @from_browser = false
 
     mail(to: @beta_requester.email, subject: 'Welcome to Draft Private Beta')
   end
