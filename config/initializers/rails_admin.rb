6425ead770c4abc4d93db1bdd17eb8f321118fd6
Islam Wazery <wazery@ubuntu.com>
2016-10-22 12:06:42 +0200

    Fix Kaminari conflict with WillPaginate

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -45,3 +45,18 @@ RailsAdmin.config do |config|
     # history_show
   end
 end
+
+# To fix Kaminari from RailsAdmin conflict with WillPaginate
+if defined?(WillPaginate)
+  module WillPaginate
+    module ActiveRecord
+      module RelationMethods
+        def per(value = nil) per_page(value) end
+        def total_count() count end
+      end
+    end
+    module CollectionMethods
+      alias_method :num_pages, :total_pages
+    end
+  end
+end
