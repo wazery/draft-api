e8ff8c57d99bb18744e9d2281b63ba84483f739a
Islam Wazery <wazery@ubuntu.com>
2016-10-23 19:12:32 +0200

    Add docs for tags endpoints

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,19 +1,59 @@
 class TagsController < BaseController
   before_action :set_tag, only: %i(show update destroy)
 
-  # GET /tags
+  ################# Documentation ##############################################
+  api :GET, '/projects/:project_id/artboards/:artboard_id/tags', 'Returns all tags for artboards'
+  example <<-EOS
+    [
+      {
+        id:
+        name:
+      }
+    ]
+  EOS
+  param :artboard_id, Integer, desc: 'Artboard ID', required: true
+  error code: 401, desc: 'Authentication failed'
+  error code: 404, desc: 'Artboard not found'
+  ################# /Documentation #############################################
   def index
     @tags = Artboard.find(params[:artboard_id]).tags
 
     render json: @tags.decorate.to_json
   end
 
-  # GET /tags/1
+  ################# Documentation ##############################################
+  api :GET, '/projects/:project_id/artboards/:artboard_id/tags/:id', 'Returns the created tags or the errors'
+  example <<-EOS
+    {
+      id:
+      name:
+    }
+  EOS
+  param :artboard_id, Integer, desc: 'Artboard ID', required: true
+  error code: 400, desc: 'Bad request, when empty project hash is passed'
+  error code: 401, desc: 'Authentication failed'
+  error code: 404, desc: 'Project not found'
+  ################# /Documentation #############################################
   def show
     render json: @tag
   end
 
-  # POST /tags
+  ################# Documentation ##############################################
+  api :POST, '/projects/:project_id/artboards/:artboard_id/tags', 'Returns the created tags or the errors'
+  example <<-EOS
+    {
+      id:
+      name:
+    }
+  EOS
+  param :artboard_id, Integer, desc: 'Artboard ID', required: true
+  param :tag, Hash, required: true do
+    param :name, String, desc: 'Tag name', required: true
+  end
+  error code: 400, desc: 'Bad request, when empty project hash is passed'
+  error code: 401, desc: 'Authentication failed'
+  error code: 404, desc: 'Project not found'
+  ################# /Documentation #############################################
   def create
     @tag = Tag.find_or_create_by(name: tag_params[:name],
                                  artboard_id: params[:artboard_id])
@@ -21,7 +61,22 @@ class TagsController < BaseController
     render json: @tag.decorate.to_json, status: :ok
   end
 
-  # PATCH/PUT /tags/1
+  ################# Documentation ##############################################
+  api :POST, '/projects/:project_id/artboards/:artboard_id/tags', 'Returns the updated tags or the errors'
+  example <<-EOS
+    {
+      id:
+      name:
+    }
+  EOS
+  param :artboard_id, Integer, desc: 'Artboard ID', required: true
+  param :tag, Hash, required: true do
+    param :name, String, desc: 'Tag name', required: true
+  end
+  error code: 400, desc: 'Bad request, when empty project hash is passed'
+  error code: 401, desc: 'Authentication failed'
+  error code: 404, desc: 'Tag not found'
+  ################# /Documentation #############################################
   def update
     if @tag.update(tag_params)
       render json: @tag
@@ -30,7 +85,11 @@ class TagsController < BaseController
     end
   end
 
-  # DELETE /tags/1
+  ################# Documentation ##############################################
+  api :DELETE, '/projects/:project_id/artboards/:artboard_id/tags/:id', 'Does not return anything'
+  error code: 401, desc: 'Authentication failed'
+  error code: 404, desc: 'Tag not found'
+  ################# /Documentation #############################################
   def destroy
     @tag.destroy
   end
