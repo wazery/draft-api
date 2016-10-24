d1caa45b776f7385809da7f49b7e9bea81b3bf30
Islam Wazery <wazery@ubuntu.com>
2016-10-25 00:53:41 +0200

    Fix a simple typo in taggings

--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -1,4 +1,4 @@
 class Tagging < ActiveRecord::Base
   belongs_to :tag
-  belong_to :taggable, polymorphic: true
+  belongs_to :taggable, polymorphic: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161024214344) do
+ActiveRecord::Schema.define(version: 20161024224451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -104,6 +104,12 @@ ActiveRecord::Schema.define(version: 20161024214344) do
     t.datetime "updated_at",                  null: false
     t.integer  "status",          default: 0
     t.index ["slug"], name: "index_projects_on_slug", using: :btree
+  end
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer "tag_id"
+    t.string  "taggable_type"
+    t.integer "taggable_id"
   end
 
   create_table "tags", force: :cascade do |t|
