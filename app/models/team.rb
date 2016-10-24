970518130a06101ffbfedeb993a2537c68c1b3e3
Islam Wazery <wazery@ubuntu.com>
2016-10-25 01:22:23 +0200

    Fix memberships relation destroying

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,7 +1,7 @@
 class Team < ApplicationRecord
   # Relations
   has_many :invites
-  has_many :memberships
+  has_many :memberships, dependent: :destroy
   has_many :users, through: :memberships
 
   belongs_to :project
