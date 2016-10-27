847a6ac7e8a54766720279e336068e5b6fd2c785
Islam Wazery <wazery@ubuntu.com>
2016-10-27 12:59:25 +0200

    Fix duplicate line in aws initializer

--- config/initializers/aws-s3.rb
+++ config/initializers/aws-s3.rb
@@ -1,7 +1,6 @@
 require 'aws-sdk'
 
 Aws.config.update({
-  region: 'eu-central-1',
   region: ENV['AWS_REGION'],
   credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
 })
