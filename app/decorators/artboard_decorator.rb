bab2a441be7c3e8f6bf9bfcb1e0f3db50f6c92dc
Islam Wazery <wazery@ubuntu.com>
2016-10-28 02:23:12 +0200

    Fix artboard images URLs

--- app/decorators/artboard_decorator.rb
+++ app/decorators/artboard_decorator.rb
@@ -5,19 +5,13 @@ class ArtboardDecorator < Draper::Decorator
     ret = as_json(except: %i(artboardImageContentType
                              artboardImageFileName artboardImageFileSize artboardImageUpdatedAt))
 
-    ret[:full_image]  = attachment_url(artboard_image, :large)
-    ret[:thumb_image] = attachment_url(artboard_image, :thumb)
+    ret[:full_image]  = artboard_image.url(:large)
+    ret[:thumb_image] = artboard_image.url(:thumb)
     ret[:layers]      = layers
     ret[:notes]       = notes.decorate.to_json
     ret[:slices]      = slices
     ret[:exportables] = exportables
 
     ret
-  end
-
-  private
-
-  def attachment_url(file, style = :original)
-    "#{h.request.protocol}#{h.request.host_with_port}#{file.url(style)}"
   end
 end
