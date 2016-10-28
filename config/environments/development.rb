1b7f633bc8c5c4c8433213e772f45cf9b6dce495
Islam Wazery <wazery@ubuntu.com>
2016-10-28 07:46:49 +0200

    Add AWS S3 configs to dev env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,4 +52,16 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
   config.action_mailer.delivery_method = :smtp
+  #
+  # Amazon S3 and Paperclip configurations
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_host_name: 's3.eu-central-1.amazonaws.com',
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
+    }
+  }
 end
