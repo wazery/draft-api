f74161774a194a0907957b1a93ebe5338726a0ff
Islam Wazery <wazery@ubuntu.com>
2016-11-04 13:03:07 +0200

    Add artboards count to the project to_json

--- app/decorators/project_decorator.rb
+++ app/decorators/project_decorator.rb
@@ -2,13 +2,14 @@ class ProjectDecorator < Draper::Decorator
   delegate_all
 
   def to_json(options = {})
-    ret             = as_json
-    ret[:artboards] = artboards.paginate(page: options[:page]).decorate.to_json
-    ret[:slices]    = slices # FIXME: Needs to be moved to artboards
-    ret[:colors]    = colors
+    ret                   = as_json
+    ret[:artboards]       = artboards.paginate(page: options[:page]).decorate.to_json
+    ret[:artboards_count] = artboards_count
+    ret[:slices]          = slices # FIXME: Needs to be moved to artboards
+    ret[:colors]          = colors
 
-    ret[:tags]      = tags.decorate.to_json
-    ret[:team]      = team.decorate.to_json if team.present?
+    ret[:tags]            = tags.decorate.to_json
+    ret[:team]            = team.decorate.to_json if team.present?
 
     ret
   end
