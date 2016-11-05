5fa8583074d19dd21b8b366fca088e28f9492540
Islam Wazery <wazery@ubuntu.com>
2016-11-05 16:50:01 +0200

    Remove un-necessary endpoint from misc controller

--- app/controllers/misc_controller.rb
+++ app/controllers/misc_controller.rb
@@ -2,9 +2,4 @@ class MiscController < ApplicationController
   def ping
     render json: { status: 'OK' }
   end
-
-  def pong
-    # byebug
-    render json: { status: 'OK'}, status: 404
-  end
 end
