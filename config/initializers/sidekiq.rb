0f8d3061dc356184eafcb2b25ef128814564e2ca
Islam Wazery <wazery@ubuntu.com>
2016-11-06 01:52:49 +0200

    Create sidekiq.rb

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,7 @@
+Sidekiq.configure_server do |config|
+  config.redis = { url: "redis://#{ ENV['REDIS_SERVICE_HOST' }:#{ ENV['REDIS_SERVICE_PORT'] }/0" }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: "redis://#{ ENV['REDIS_SERVICE_HOST' }:#{ ENV['REDIS_SERVICE_PORT'] }/0" }
+end
