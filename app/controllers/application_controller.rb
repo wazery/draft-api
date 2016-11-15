5de679cfe33219825802e7e346ad492685f5bd09
Islam Wazery <wazery@ubuntu.com>
2016-11-15 22:21:23 +0200

    Permit firstname and lastname in Signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i(email password name))
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i(email password firstname lastname name))
   end
 end
