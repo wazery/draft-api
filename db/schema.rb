672574a1b0bcbb4bc045c7079bf9fcf972cb6dcf
Islam Wazery <wazery@ubuntu.com>
2016-11-26 22:40:35 +0200

    Update missing schema after tags color migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161028010418) do
+ActiveRecord::Schema.define(version: 20161124174735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -144,6 +144,7 @@ ActiveRecord::Schema.define(version: 20161028010418) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "color"
   end
 
   create_table "teams", force: :cascade do |t|
