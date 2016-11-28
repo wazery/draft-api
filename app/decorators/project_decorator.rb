6569dfc011df2fd512076cfeed556e5dc255c3d2
Islam Wazery <wazery@ubuntu.com>
2016-11-28 22:30:01 +0200

    Fix project decorator

--- app/decorators/project_decorator.rb
+++ app/decorators/project_decorator.rb
@@ -5,7 +5,7 @@ class ProjectDecorator < Draper::Decorator
     ret                   = as_json
     ret[:artboards]       = artboards.paginate(page: options[:page]).decorate.to_json
     ret[:artboards_count] = artboards_count
-    ret[:styleguide_id]   = styleguide.id
+    ret[:styleguide_id]   = styleguide.id if styleguide
     ret[:slices]          = slices # FIXME: Needs to be moved to artboards
     ret[:colors]          = colors
 
