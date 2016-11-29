867446e80d3ce08b843a4ccb386511f998c2b2eb
Islam Wazery <wazery@ubuntu.com>
2016-11-30 01:10:58 +0200

    Return role with the user

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -2,7 +2,7 @@ class UserDecorator < Draper::Decorator
   delegate_all
 
   def to_index_json
-    ret = as_json(only: %i(email name firstname lastname image))
+    ret = as_json(only: %i(email name firstname lastname image role))
     ret[:name] = firstname + ' ' + lastname if firstname && lastname && !name
 
     ret
