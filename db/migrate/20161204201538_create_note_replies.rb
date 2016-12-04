c8408dd44446a4d33417d49e2bf3e800064536ab
Islam Wazery <wazery@ubuntu.com>
2016-12-04 22:59:49 +0200

    Fix note replies migration

--- db/migrate/20161204201538_create_note_replies.rb
+++ db/migrate/20161204201538_create_note_replies.rb
@@ -2,7 +2,7 @@ class CreateNoteReplies < ActiveRecord::Migration[5.0]
   def change
     create_table :note_replies do |t|
       t.string :text
-      t.reference :note_id
+      t.references :note
 
       t.timestamps
     end
