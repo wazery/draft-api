384ae35ced4439cff8716a1625b02234c4ebf79a
Islam Wazery <wazery@ubuntu.com>
2016-12-05 03:45:48 +0200

    Change the content name of the note reply

--- app/decorators/note_reply_decorator.rb
+++ app/decorators/note_reply_decorator.rb
@@ -2,9 +2,7 @@ class NoteReplyDecorator < Draper::Decorator
   delegate_all
 
   def to_json
-    ret = as_json(only: %i(id created_at updated_at))
-
-    ret[:note] = text
+    ret = as_json(only: %i(id text created_at updated_at))
 
     reply_user = user
     if reply_user
