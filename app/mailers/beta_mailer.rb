c676ab5c238c8e0c657c68319a01063bba4a478b
Islam Wazery <wazery@ubuntu.com>
2016-12-28 22:13:05 +0200

    Add invite mailer and email

--- app/mailers/beta_mailer.rb
+++ app/mailers/beta_mailer.rb
@@ -5,4 +5,11 @@ class BetaMailer < ApplicationMailer
 
     mail(to: @beta_requester.email, subject: 'Welcome to Draft Private Beta')
   end
+
+  def invite_email(beta_requester)
+    @beta_requester = beta_requester
+    @from_browser = false
+
+    mail(to: @beta_requester.email, subject: 'Your invitation to Draft App beta test is here!')
+  end
 end
