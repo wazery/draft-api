fb758bcbcf0f23b735f11af842ed9900b33e2dc2
Islam Wazery <wazery@ubuntu.com>
2017-01-12 07:20:33 +0200

    Fix ID

--- app/models/user.rb
+++ app/models/user.rb
@@ -50,7 +50,7 @@ class User < ActiveRecord::Base
 
   # FIXME: Very dirty code to duplicate demo project
   def create_demo_project
-    project = Project.find(81)
+    project = Project.find(82)
 
     duplicate_project = project.deep_clone include: [ { artboards: :artboard_image_attachment }, :attachments]
     duplicate_project.save
